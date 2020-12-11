fb077cd3b4e1103fb459a3f7b1fc7524a1b3ec1f
leviethung123 <levan999mt@gmail.com>
2020-12-11 21:31:15 +0700

    update thong ke

--- src/app/thongKe/thongKe.component.ts
+++ src/app/thongKe/thongKe.component.ts
@@ -16,6 +16,9 @@ export class thongKeComponent implements OnInit {
   ngOnInit(): void {
     this.ThongkeService.getthongke().subscribe(data => {
       this.thongke = data;
+      this.thongke = data.filter((value,i) => i <= 3);
+      this.thongke = this.shuffle(this.thongke);
+
       console.log(data);
       
       this.barChartData = [
@@ -54,4 +57,23 @@ export class thongKeComponent implements OnInit {
   barChartPlugins = [];
 
   barChartData: ChartDataSets[];
+
+  shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+   
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+   
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+   
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+   
+    return array;
+  }
 }
