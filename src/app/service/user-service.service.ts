be77bea595b3f702fef714137ffea1891350bf92
unknown <exfick@gmail.com>
2020-12-13 21:27:37 +0700

    edit user

--- src/app/service/user-service.service.ts
+++ src/app/service/user-service.service.ts
@@ -10,31 +10,34 @@ export class UserServiceService {
   static getUserByEmail(email_user: string) {
     throw new Error('Method not implemented.');
   }
-  private baseUrl = 'http://localhost:8000/greenmarket/users/users';
+  private baseUrlUse = 'http://localhost:8000/greenmarket/users/users';
+  // private baseurlPro = 'http://localhost:8000/greenmarket/api/mathang';
 
   constructor(private http: HttpClient) { }
 
   createUser(Users: Object): Observable<Object> {
-    return this.http.post(`${this.baseUrl}`, Users);
+    return this.http.post(`${this.baseUrlUse}`, Users);
   }
-
-  getUserList(): Observable<any> {
-    return this.http.get(`${this.baseUrl}`);
+  // getProductList(): Observable<any> {
+  //   return this.http.get(`${this.baseurlPro}`);
+  // }
+  getUsList(link): Observable<any> {
+    return this.http.get(`${link}`);
   }
 
   getUserById(id: any): Observable<any> {
-    return this.http.get(`${this.baseUrl}/${id}`);
+    return this.http.get(`${this.baseUrlUse}/${id}`);
   }
 
   getUserByEmail(email: String): Observable<any> {
-    return this.http.get(`${this.baseUrl}/email/${email}`);
+    return this.http.get(`${this.baseUrlUse}/email/${email}`);
   }
   updateUser(id: any, value: any): Observable<Object> {
-    return this.http.put(`${this.baseUrl}/${id}`, value);
+    return this.http.put(`${this.baseUrlUse}/${id}`, value);
   }
 
   deleteUser(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+    return this.http.delete(`${this.baseUrlUse}/${id}`, { responseType: 'text' });
   }
 
 
